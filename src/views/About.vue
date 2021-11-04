027981afbcfe600a24af7dcbb838a02ea763fa2a
Tim Home <a0963573232@gmail.com>
2021-11-05 02:15:09 +0800

    Style: 設定網站色系

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <NearbyInfo></NearbyInfo>
+    <!-- <NearbyInfo></NearbyInfo> -->
   </div>
 </template>
 
 <script>
-import { getTravelInfo } from "../modules.js";
-import NearbyInfo from "../components/NearbyInfo.vue";
+// import { getTravelInfo } from "../modules.js";
+// import NearbyInfo from "../components/NearbyInfo.vue";
 
 export default {
   name: "About",
-  components: { NearbyInfo },
+  // components: { NearbyInfo },
   setup() {
-    getTravelInfo("ScenicSpot").then((res) => console.log(res));
+    // getTravelInfo("ScenicSpot").then((res) => console.log(res));
     // getTravelInfo("Restaurant").then((res) => console.log(res));
     // getTravelInfo("Hotel").then((res) => console.log(res));
     // getTravelInfo("Activity").then((res) => console.log(res));
