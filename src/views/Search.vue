54bb4a59b315b74cd8a0509290ac351b83a2ad14
Tim Home <a0963573232@gmail.com>
2021-11-05 20:44:03 +0800

    Feat: Search Page 參數取得

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,27 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <div v-text="params"></div>
+  </div>
 </template>
 
 <script>
-// import { computed, ref, watch } from "@vue/runtime-core";
+import { computed } from "@vue/runtime-core";
 import { useRoute } from "vue-router";
-// import { getDetail } from "../modules.js";
 
 export default {
   name: "Search",
   setup() {
     const route = useRoute();
-    console.log(route.params);
-    // const ID = computed(() => route.params.ID);
-    // const detail = ref(null);
-    // getDetail(ID.value).then((res) => (detail.value = res[0]));
+    const params = computed(() => route.params);
 
-    // watch(ID, () => {
-    //   if (!ID.value) return;
-    //   getDetail(ID.value).then((res) => (detail.value = res[0]));
-    // });
-
-    // return { ID, detail };
+    return { params };
   },
 };
 </script>
