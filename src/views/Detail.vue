aa1e762bf7f9a8e6b50665d3156a7a6ecf77cad6
Tim Home <a0963573232@gmail.com>
2021-11-05 17:08:18 +0800

    Feat: 監控 ID 變化以載入資料

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,16 +1,27 @@
 <template>
   <div>Detail</div>
+  <h2 v-text="ID"></h2>
+  <div v-text="detail"></div>
 </template>
 
 <script>
+import { computed, ref, watch } from "@vue/runtime-core";
 import { useRoute } from "vue-router";
 import { getDetail } from "../modules.js";
 
 export default {
   name: "Detail",
   setup() {
-    const ID = useRoute().params.ID;
-    getDetail(ID).then((res) => console.log(res[0]));
+    const route = useRoute();
+    const ID = computed(() => route.params.ID);
+    const detail = ref(null);
+    getDetail(ID.value).then((res) => (detail.value = res[0]));
+
+    watch(ID, () => {
+      getDetail(ID.value).then((res) => (detail.value = res[0]));
+    });
+
+    return { ID, detail };
   },
 };
 </script>
